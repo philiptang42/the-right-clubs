ce37ead70e40fd352999f10ef4e609f9249ed3c9
philiptang42 <philiptang42@gmail.com>
2015-11-24 17:08:40 -0500

    part 1 start

---
+++ the_right_clubs.rb
@@ -0,0 +1,21 @@
+REQUIRED_BAG_SIZE = 10
+
+available_clubs = [
+  :two_iron,
+  :three_iron,
+  :four_iron,
+  :five_iron,
+  :six_iron,
+  :seven_iron,
+  :eight_iron,
+  :nine_iron,
+  :pitching_wedge,
+  :sand_wedge,
+  :driver,
+  :three_wood,
+  :five_wood,
+  :seven_wood,
+  :putter
+]
+
+possible_club_selections = available_clubs.sample(REQUIRED_BAG_SIZE)
